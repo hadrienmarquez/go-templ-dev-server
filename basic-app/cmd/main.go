d88a7caffeab663a69efa983beb5b4a6448c4616
Hadrien Marquez <hadrienmarquez@gmail.com>
2024-03-07 15:35:21 +0100

    Entrypoint server for this basic app

--- basic-app/cmd/main.go
+++ basic-app/cmd/main.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	handler "github.com/hadrienmarquez/go-templ-dev-server/basic-app/api/handlers"
 )
 
 func main()  {
-	
+
+	address :=  "localhost:8080"
+	mux :=  http.NewServeMux()
+
+	mux.HandleFunc("/", handler.DefaultHandler)
+
+	fmt.Printf("Listening at %s", address)
+	http.ListenAndServe(address, mux)
 }
